33926e5334034a8e6cccddbf251072d1eda6bafd
Cédric N <nocenti.cedric@gmail.com>
2018-09-27 16:02:06 +0200

    Ajoutez des fichiers projet.

---
+++ JC_PROJECT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JC_PROJECT.Startup))]
+namespace JC_PROJECT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
