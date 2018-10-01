8c9bb1df3fe5a039bc8d199624a7111c00d8ad0d
gprog39 <pyfes@aol.com>
2018-10-01 23:01:02 +0200

    ajout DisplayAllProducts, modification écran accueil et affichage produits

--- JC_PROJECT/Controllers/ProductController.cs
+++ JC_PROJECT/Controllers/ProductController.cs
@@ -20,6 +20,38 @@ namespace JC_PROJECT.Controllers
             return View();
         }
 
+        //Afficher tous les produits, peu importe le magasin 
+        public async Task<ActionResult> DisplayAllProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/product/");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    products = JsonConvert.DeserializeObject<List<Product>>(EmpResponse);
+
+                }
+                //returning the product list to view  
+                return View(products);
+            }
+        }
         // GET: Product/Details/5
         public async Task<ActionResult> Details(int id)
         {
@@ -117,5 +149,6 @@ namespace JC_PROJECT.Controllers
                 return View();
             }
         }
+        
     }
 }
